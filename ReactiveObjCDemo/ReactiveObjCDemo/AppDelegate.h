e4c013a85716eeb11e22ff16dd6ef91558a6fc35
liujiajie <liujiajie@dianping.com>
2018-08-20 21:37:27 +0800

    【New Project】

---
+++ ReactiveObjCDemo/ReactiveObjCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactiveObjCDemo
+//
+//  Created by 刘佳杰 on 2018/8/20.
+//  Copyright © 2018年 Jiajie.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReactiveObjCDemo/ReactiveObjCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactiveObjCDemo
+//
+//  Created by 刘佳杰 on 2018/8/20.
+//  Copyright © 2018年 Jiajie.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
